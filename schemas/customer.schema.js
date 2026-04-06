b99643c017b5180c4ad1b48ed3e47bfda31ec4de
Fernando Orozco <jose.acciones.gt@gmail.com>
2026-04-06 15:34:54 -0600

    Stablished first one to one relation using Sequelize for users and customers tables

---
+++ schemas/customer.schema.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string().min(3).max(30);
+const lastname = Joi.string();
+const phone = Joi.string();
+const userId = Joi.number().integer();
+
+const getCustomerSchema = Joi.object({
+  id: id.required(),
+});
+
+const createCustomerSchema = Joi.object({
+  name: name.required(),
+  lastName: lastname.required(),
+  phone: phone.required(),
+  userId: userId.required(),
+});
+
+const updateCustomerSchema = Joi.object({
+  name,
+  lastname,
+  phone,
+  userId,
+});
+
+module.exports = { getCustomerSchema, createCustomerSchema, updateCustomerSchema }
---
+++ services/customers.service.js
@@ -0,0 +1,41 @@
+const boom = require('@hapi/boom');
+const { models } = require('./../libs/sequelize');
+
+class CustomerService {
+  constructor() {
+
+  }
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async find() {
+    const res = await models.Customer.findAll()
+    return res;
+  }
+
+  async findOne(id) {
+    const customer = await models.Customer.findByPk(id);
+    if (!customer) {
+      throw boom.notFound('customer not found');
+    }
+    return customer;
+  }
+
+  async update(id, changes) {
+    const model = await this.findOne(id);
+    const res = await model.update(changes);
+    return res;
+  }
+
+  async delete(id) {
+    const model = await this.findOne(id);
+    await model.destroy();
+    return { res: true };
+  }
+
+}
+
+module.exports = CustomerService;
